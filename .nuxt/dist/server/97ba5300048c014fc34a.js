f4d6f644583d10a1019715bfeed0c6b07c1db643
falstack <icesilt@outlook.com>
2020-03-05 14:40:25 +0000

    Add changes

---
+++ .nuxt/dist/server/97ba5300048c014fc34a.js
@@ -0,0 +1,1 @@
+exports.ids=[52],exports.modules={390:function(e,t,r){"use strict";r.r(t);var l={asyncData({redirect:e,route:t}){e(`${t.path.replace(/\/$/,"")}/following`,t.query)}},n=r(1),component=Object(n.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("div",[])}),[],!1,null,null,"945d62b6");t.default=component.exports}};
