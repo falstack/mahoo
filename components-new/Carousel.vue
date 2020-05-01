dca27a1a18a0f8937d3a6381426ade6eeabf1f03
falstack <icesilt@outlook.com>
2020-05-01 14:32:52 +0800

    save

--- components-new/Carousel.vue
+++ components-new/Carousel.vue
@@ -1,33 +1,95 @@
 <style lang="scss">
-#v-carousel {
+.v-switcher {
   width: 440px;
   height: 220px;
-}
+  border-radius: 4px;
+  overflow: hidden;
 
-.v-swipe {
-  &__indicator {
-    width: 100%;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-image: -webkit-linear-gradient(transparent,rgba(0,0,0,.5));
-    height: 35px;
-    line-height: 35px;
-    font-size: 14px;
-    text-align: right;
-    padding-bottom: 6px;
-    padding-right: 20px;
-    pointer-events: auto;
+  .carousel-item {
+    display: block;
+    height: 220px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
 
-    &__item {
+    .title {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      height: 35px;
+      line-height: 35px;
+      display: block;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      color: #fff;
+      font-size: 15px;
+      text-decoration: none;
+      background: -webkit-linear-gradient(transparent, rgba(0, 0, 0, 0.5));
+      padding-left: 10px;
+      padding-right: 160px;
+    }
+
+    .more {
+      display: block;
+      position: absolute;
+      right: 15px;
+      bottom: 35px;
+      color: #fff;
+      background-color: rgba(0, 0, 0, 0.64);
+      width: 50px;
+      height: 24px;
+      line-height: 24px;
+      text-align: center;
+      border-radius: 4px;
+      opacity: 0;
+      transition: all 0.2s linear;
+      font-size: 12px;
+      text-decoration: none;
+
+      &:hover {
+        text-shadow: 0 0 3px #fff;
+      }
+
+      &:after {
+        content: '';
+        display: inline-block;
+        width: 6px;
+        height: 12px;
+        margin: -2px 0 0 5px;
+        vertical-align: middle;
+        background: url(~assets/img/icons.png) -541px -218px;
+      }
+    }
+
+    &:hover .more {
+      opacity: 1;
+    }
+  }
+
+  &-header {
+    &-wrap {
+      box-sizing: border-box;
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      height: 27px !important;
+      z-index: 1;
+      width: 150px;
+      padding-right: 20px;
+      border-bottom: none;
+    }
+
+    &-item {
+      margin-left: 8px;
       width: 18px;
       height: 18px;
-      background-image: url(~assets/img/icons.png);
       background-position: -855px -790px;
-      background-color: transparent;
-      transition: none;
+      background-image: url(~assets/img/icons.png);
+      border-bottom-width: 0;
       cursor: pointer;
-      margin-left: 8px;
+      padding: 0;
 
       &:hover {
         background-position: -919px -790px;
@@ -35,9 +97,6 @@
 
       &.is-active {
         background-position: -855px -727px;
-        width: 18px;
-        height: 18px;
-        background-color: transparent;
       }
     }
   }
@@ -45,32 +104,59 @@
 </style>
 
 <template>
-  <VSwipe id="v-carousel" :length="3" @change="handleChange" @report="handleReport">
-    <div slot="0" style="background-color: #6dc781">
-      0
-    </div>
-    <div slot="1" style="background-color: #cc99cd">
-      1
-    </div>
-    <div slot="2" style="background-color: #6a8bad">
-      2
-    </div>
-  </VSwipe>
+  <VSwitcher :headers="headers" :swipe="true" :autoplay="2000" align="end">
+    <a
+      v-for="(item, index) in headers"
+      :key="index"
+      :slot="index"
+      :style="{ backgroundColor: item.color }"
+      class="carousel-item"
+      href="javascript:;"
+    >
+      <a class="title" href="javascript:;">{{ item.title }}</a>
+      <a class="more" href="javascript:;">更多</a>
+    </a>
+  </VSwitcher>
 </template>
 
 <script>
-import { VSwipe } from '@calibur/sakura'
-
 export default {
   name: 'VCarousel',
-  components: {
-    VSwipe
-  },
   props: {},
   data() {
     return {}
   },
-  computed: {},
+  computed: {
+    headers() {
+      return [
+        {
+          text: '',
+          title: '成名必备！',
+          color: 'rgba(21,174,103,.5)'
+        },
+        {
+          text: '',
+          title: '花泽香菜，甜美来袭！',
+          color: 'rgba(255,230,135,.5)'
+        },
+        {
+          text: '',
+          title: '鸡鸣紫陌曙光寒，水转皇州春色阑',
+          color: 'rgba(195,123,177,.5)'
+        },
+        {
+          text: '',
+          title: '请查收您的追番清单!',
+          color: 'rgba(125,205,244,.5)'
+        },
+        {
+          text: '',
+          title: '欢迎来到天生制造狂的世界',
+          color: 'rgba(195,123,177,.5)'
+        }
+      ]
+    }
+  },
   watch: {},
   created() {},
   mounted() {},
