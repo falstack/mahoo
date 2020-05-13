bacf997622c7d6b9ce3b759a5d07a14f9d4ab7ad
falstack <icesilt@outlook.com>
2020-05-13 20:16:33 +0800

    save

--- pages/user/_slug/setting/spider.vue
+++ pages/user/_slug/setting/spider.vue
@@ -1,9 +1,11 @@
 <style lang="scss">
 #user-spider-setting {
   .rule-item {
+    position: relative;
     border: 1px solid $color-gray-line;
     padding: 15px;
     border-radius: 5px;
+    margin-bottom: 15px;
 
     .opt-item {
       display: flex;
@@ -17,6 +19,12 @@
       &.type {
         margin-bottom: 10px;
       }
+    }
+
+    .close-btn {
+      position: absolute;
+      right: 15px;
+      top: 5px;
     }
 
     .el-input,
@@ -35,13 +43,32 @@
       margin-top: 5px;
     }
   }
+
+  .submit-row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
 }
 </style>
 
 <template>
   <div v-if="isAuth" id="user-spider-setting">
     <template v-if="user_bilibili_mid">
-      <div class="rule-item" v-for="item in rule" :key="item.key">
+      <ElAlert title="什么是频道id？" type="success">
+        <span>比如：</span>
+        <a target="_blank" href="https://space.bilibili.com/279662469/channel/detail?cid=118679">https://space.bilibili.com/279662469/channel/detail?cid=118679</a>
+        <span>这个链接中，频道部分就是cid=118679，那么频道的 id 就是</span>
+        <span><b>118679</b></span>
+      </ElAlert>
+      <br>
+      <ElAlert title="请勿频繁的修改规则" type="warning">
+        如果你删除规则，calibur 也会删除对应的内容，但这个过程需要几分钟至几小时的同步时间
+      </ElAlert>
+      <br>
+      <div class="rule-item" v-for="(item, index) in rule" :key="item.key">
+        <ElButton class="close-btn" type="text" icon="el-icon-close" @click="deleteRuleItem(index)" />
         <p class="opt-item type">
           <span class="label">类型：</span>
           <span class="value" v-text="convertSourceType(item.type)" />
@@ -75,6 +102,10 @@
             @keyup.enter.native="handleInputConfirm(item)"
           />
         </p>
+      </div>
+      <div class="submit-row">
+        <ElButton icon="el-icon-plus" circle @click="appendRule" />
+        <ElButton :loading="submitting" plain type="success" @click="saveRule">确认保存</ElButton>
       </div>
     </template>
     <ElAlert v-else-if="user_bilibili_mid === null" title="请先在「认证设置」页面绑定你的B站账号" type="success" />
@@ -117,6 +148,38 @@ export default {
     this.getUserRule()
   },
   methods: {
+    appendRule() {
+      this.rule.push({
+        key: Math.random().toString(36).substring(3, 6),
+        channel_id: [],
+        type: 'video',
+        source_type: 1,
+        new: ''
+      })
+    },
+    deleteRuleItem(index) {
+      this.rule.splice(index, 1)
+    },
+    saveRule() {
+      if (this.submitting) {
+        return
+      }
+      this.submitting = true
+      this.$axios.$post('v1/console/spider/save_user_rule', {
+        channel: 'bilibili',
+        rule: this.rule
+      })
+        .then(() => {
+          this.$toast.success('设置成功！')
+            .then(() => {
+              window.location.reload()
+            })
+        })
+        .catch((err) => {
+          this.submitting = false
+          this.$toast.error(err.message)
+        })
+    },
     handleClose(item, index) {
       item.channel_id.splice(index, 1)
     },
