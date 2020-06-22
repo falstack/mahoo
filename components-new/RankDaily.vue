d24cbd5620c38a0b18bc30ef5adca9bd4d2fd386
falstack <icesilt@outlook.com>
2020-06-22 08:54:22 +0800

    save

---
+++ components-new/RankDaily.vue
@@ -0,0 +1,22 @@
+<style lang="scss">
+</style>
+
+<template>
+  <div class="rank-daily" />
+</template>
+
+<script>
+export default {
+  name: 'RankDaily',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
